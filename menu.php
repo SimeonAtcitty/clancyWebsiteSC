819218b10b2710ed217444c8da3e114f884a8863
SimeonAtcitty <atcittysimo@gmail.com>
2016-10-13 13:54:44 -0600

    added image to menu

--- menu.php
+++ menu.php
@@ -4,10 +4,17 @@
     <meta charset="utf-8">
     <title>Menu</title>
     <?php require_once("lib/headutils.php");?>
+    <link rel="stylesheet" href="lib/css/menu.css">
   </head>
   <body>
     <?php require_once("lib/partials/navBar.php");?>
 
+    <div class="jumbotron" id="jumbotron1">
+      <h1 id="headText">Menu Items</h1>
+      <p>
+        Take a look around of our awesome selection.
+      </p>
+    </div>
 
   </body>
 </html>
