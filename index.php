a859bb3bd552af50df10ae10c29f7dfd0febcc27
chenoajai <winsteadchenoa@gmail.com>
2016-10-14 11:36:12 -0600

    added stuff

--- index.php
+++ index.php
@@ -8,132 +8,157 @@
   </head>
 
   <body>
+
     <?php require_once("lib/partials/navBar.php");?>
 
     <div class="jumbotron" id="jumbotron1">
       <h1 id="headText">Clancy's</h1>
         <p> Irish Pub House</p>
     </div>
+
     <div>
         <h2 id="headp2">Food and Spirits</h2>
     </div>
 
 
     <div class="container">
-        <div class="row">
-          <div class="col-md-5">
-            <h3>Best Irish Beer in Town</h3>
+      <div class="row">
+        <div class="col-md-6">
+          <h3>Best Irish Beer in Town</h3>
           <hr>
-            <p>With a focus on Irish brands, and 24 taps dedicated to the best of Irish & International craft Beer,
-              our aim is to provide the highest quality products available, including a large range
-              of independent Irish Gin & Whiskey,and we can safely say you won’t be stuck for choice.
+          <p>With a focus on Irish brands, and 24 taps dedicated to the best of Irish & International craft Beer,
+            our aim is to provide the highest quality products available, including a large range
+            of independent Irish Gin & Whiskey,and we can safely say you won’t be stuck for choice.
+          </p>
+          <img class="bar" src="http://tinyurl.com/hjfm3mp" alt="" />
+          <hr>
 
-            <img class="bar" src="http://tinyurl.com/hjfm3mp" alt="" />
+        </div>
 
-        <hr>
+        <div class="col-md-6">
+          <h3> Try our Authentic Irish Cuisine</h3>
+          <hr>
+          <p>Our chefs are constantly working to create new and exciting dishes using in season,
+             local produce on a constantly rotating menu as well as staples that keep customers coming back time after time. You are sure to find something to suit your taste!
+             Don’t forget to ask your server about the specials too!
+           </p>
+
+          <img class="bar2" src="http://tinyurl.com/jekoll4" alt="" />
+          <hr>
+        </div>
+      </div>
     </div>
-
 
     <div class="container">
+
       <div class="row">
-      <div class="col-md-7">
-        <h3> Try our Authentic Irish Cuisine</p>
 
-        <div class="flex_column av_one_third  first el_before_av_one_third">
-          <section id="text-6" class="widget clearfix widget_text">
-
-          <h3 class="widgettitle">Opening Hours</h3>
-          <hr>
-
-          <div class="textwidget"><table class="op-table op-overview-table">
-          <tbody><tr class="op-overview-row ">
-          <th class="op-overview-title">Monday
-         </th>
-         <td class="op-overview-times">
-         <div class="op-overview-set ">
-         <strong> 12:00pm – 11:30pm  </strong>Bar
-         </div>
-         <div class="op-overview-set ">
-         <strong> 11:00am – 10:00pm </strong>Kitchen
-
-         </div>
-         </td>
-         </tr>
-
-         <tr class="op-overview-row highlighted">
-         <th class="op-overview-title">Tuesday</th>
-         <td class="op-overview-times">
-         <div class="op-overview-set ">
-         <strong>12:00pm – 11:30pm  </strong>Bar
-         </div>
-         <div class="op-overview-set ">
-           <strong> 11:00am – 10:00pm  </strong>Kitchen
-        </div>
-        </td>
-        </tr>
-        <tr class="op-overview-row ">
-        <th class="op-overview-title">Wednesday</th>
-        <td class="op-overview-times">
-        <div class="op-overview-set ">
-          <strong>12:00pm – 11:30pm </strong> Bar
-        </div>
-        <div class="op-overview-set ">
-          <strong>11:00am – 10:00pm</strong>Kitchen
-        </div>
-        </td>
-        </tr>
-        <tr class="op-overview-row ">
-        <th class="op-overview-title">Thursday</th>
-        <td class="op-overview-times">
-        <div class="op-overview-set ">
-          <strong>12:00pm – 11:30pm</strong>Bar
-        </div>
-        <div class="op-overview-set ">
-          <strong>11:00am – 10:00pm</strong>Kitchen
-        </div>
-        </td>
-
-        </tr>
-        <tr class="op-overview-row ">
-        <th class="op-overview-title">Friday</th>
-        <td class="op-overview-times">
-        <div class="op-overview-set ">
-          <strong>12:00pm – 12:30am </strong>Bar
+        <div class="col-md-6">
+          <h4> Enjoy Live Music</h4>
+          <p> Ka
         </div>
 
-        <div class="op-overview-set ">
-          <strong> 11:00am – 10:00pm </strong>Kitchen
-        </div>
-        </td>
-        </tr>
-        <tr class="op-overview-row ">
-        <th class="op-overview-title">Saturday</th>
-        <td class="op-overview-times">
-          <div class="op-overview-set ">
-          <strong> 12:00pm – 12:30am </strong>Bar
-        </div>
-        <div class="op-overview-set ">
-          <strong> 11:00pm – 10:00pm</strong>Kitchen
-        </div>
-        </td>
-        </tr>
-        <tr class="op-overview-row ">
-        <th class="op-overview-title">Sunday</th>
-        <td class="op-overview-times">
-        <div class="op-overview-set ">
-          <strong>12:00pm – 9:00pm </strong> Bar
-        </div>
-        <div class="op-overview-set ">
-          <strong>  12:00pm – 8:00pm </strong>  Kitchen
-        </div>
-        </td>
-        </tr>
+        <div class="col-md-6">
+          <div class="table flex_column av_one_third  first el_before_av_one_third">
+            <section id="text-6" class="widget clearfix widget_text">
 
-        </tbody></table></div>
-  <span class="seperator extralight-border"></span></section></div>
+              <h3 class="widgettitle">Opening Hours</h3>
 
+              <div class="textwidget">
+                <table class="op-table op-overview-table">
+                  <tbody>
+                    <tr class="op-overview-row ">
+                      <th class="op-overview-title">Monday
+                      </th>
+                      <td class="op-overview-times">
+                        <div class="op-overview-set ">
+                          <strong> 12:00pm – 11:30pm  </strong>Bar
+                        </div>
+                        <div class="op-overview-set ">
+                          <strong> 11:00am – 10:00pm </strong>Kitchen
+
+                        </div>
+                      </td>
+                    </tr>
+
+                    <tr class="op-overview-row highlighted">
+                      <th class="op-overview-title">Tuesday</th>
+                      <td class="op-overview-times">
+                        <div class="op-overview-set ">
+                          <strong>12:00pm – 11:30pm  </strong>Bar
+                        </div>
+                        <div class="op-overview-set ">
+                          <strong> 11:00am – 10:00pm  </strong>Kitchen
+                        </div>
+                      </td>
+                    </tr>
+                    <tr class="op-overview-row ">
+                      <th class="op-overview-title">Wednesday</th>
+                      <td class="op-overview-times">
+                        <div class="op-overview-set ">
+                          <strong>12:00pm – 11:30pm </strong> Bar
+                        </div>
+                        <div class="op-overview-set ">
+                          <strong>11:00am – 10:00pm</strong>Kitchen
+                        </div>
+                      </td>
+                    </tr>
+                    <tr class="op-overview-row ">
+                      <th class="op-overview-title">Thursday</th>
+                      <td class="op-overview-times">
+                        <div class="op-overview-set ">
+                          <strong>12:00pm – 11:30pm</strong>Bar
+                        </div>
+                        <div class="op-overview-set ">
+                          <strong>11:00am – 10:00pm</strong>Kitchen
+                        </div>
+                      </td>
+
+                    </tr>
+                    <tr class="op-overview-row ">
+                      <th class="op-overview-title">Friday</th>
+                      <td class="op-overview-times">
+                        <div class="op-overview-set ">
+                          <strong>12:00pm – 12:30am </strong>Bar
+                        </div>
+
+                        <div class="op-overview-set ">
+                          <strong> 11:00am – 10:00pm </strong>Kitchen
+                        </div>
+                      </td>
+                    </tr>
+                    <tr class="op-overview-row ">
+                      <th class="op-overview-title">Saturday</th>
+                      <td class="op-overview-times">
+                        <div class="op-overview-set ">
+                          <strong> 12:00pm – 12:30am </strong>Bar
+                        </div>
+                        <div class="op-overview-set ">
+                          <strong> 11:00pm – 10:00pm</strong>Kitchen
+                        </div>
+                      </td>
+                    </tr>
+                    <tr class="op-overview-row ">
+                      <th class="op-overview-title">Sunday</th>
+                      <td class="op-overview-times">
+                        <div class="op-overview-set ">
+                          <strong>12:00pm – 9:00pm </strong> Bar
+                        </div>
+                        <div class="op-overview-set ">
+                          <strong>  12:00pm – 8:00pm </strong>  Kitchen
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <span class="seperator extralight-border"></span>
+            </section>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
+
 
   </body>
 </html>
